d811e97b3cdbef65de5784002297d1e74a1a1314
Caden Feller <caden.feller@gmail.com>
2024-09-10 18:38:35 -0700

    fixed commands

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ from util.config_util import ConfigUtil
 intents = discord.Intents.default()
 intents.message_content = True;
 
-client = discord.Client(intents=intents)
+client = commands.Bot(command_prefix="<>", intents=intents)
 
 tree = app_commands.CommandTree(client)
 
 config = ConfigUtil()
 
-@tree.command(name='info', description="Sends a message containing all of the info about this bot.")
-async def info(interaction: discord.Interaction):
+@client.tree.command(name='info', description="Sends a message containing all of the info about this bot.")
+async def slash_command(interaction: discord.Interaction):
     await interaction.response.send_message(f"Bot running on server: {os.uname().nodename}")
 
 @client.event
@@ -25,7 +25,6 @@ async def on_ready():
     await tree.sync()
     print("Synced the command tree!")
 
-"""
 @client.event
 async def on_message(message: Message):
     print(f"Message from {message.author}: {message.content}")
@@ -33,9 +32,11 @@ async def on_message(message: Message):
     if message.author == client.user:
         return
 
+    await client.process_commands(message)
+
     if message.content.startswith("echo"):
         await message.channel.send(message.content[5:])
-"""
+
 bot_token = config.get_config().get("bot_token")
 if bot_token == None:
     print("No bot token found!")
