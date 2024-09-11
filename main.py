88fad42268afe2a25ba30a7fdd24f4ebfebd49df
Caden Feller <caden.feller@gmail.com>
2024-09-10 18:10:57 -0700

    initial commit - basic functionality

---
+++ main.py
@@ -0,0 +1,43 @@
+import discord
+import os
+from discord.ext import commands
+from discord.message import Message
+from discord import app_commands
+
+from util.config_util import ConfigUtil
+
+intents = discord.Intents.default()
+intents.message_content = True;
+
+client = discord.Client(intents=intents)
+
+tree = app_commands.CommandTree(client)
+
+config = ConfigUtil()
+
+@tree.command(name='info', description="Sends a message containing all of the info about this bot.")
+async def info(interaction: discord.Interaction):
+    await interaction.response.send_message(f"Bot running on server: {os.uname().nodename}")
+
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user}")
+    await tree.sync()
+    print("Synced the command tree!")
+
+"""
+@client.event
+async def on_message(message: Message):
+    print(f"Message from {message.author}: {message.content}")
+
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("echo"):
+        await message.channel.send(message.content[5:])
+"""
+bot_token = config.get_config().get("bot_token")
+if bot_token == None:
+    print("No bot token found!")
+    quit() 
+client.run(bot_token)
